6b612e33ea53c8ce2af19307a3f8746eef77e1d6
Josh Myers-Dean <joshmyersdean@yahoo.com>
2019-12-09 14:28:47 -0800

    Typo

--- day2/day2.py
+++ day2/day2.py
@@ -9,7 +9,7 @@ def partOne(nums):
             nums[nums[i+3]] = nums[nums[i+1]] + nums[nums[i+2]]
         elif nums[i] == 2:
              nums[nums[i+3]] = nums[nums[i+1]] * nums[nums[i+2]]
-        i += 4
+
 
 if __name__ == '__main__':
     with open('input.txt', 'rt') as f_obj:
