767bfd28df87719a74966478e111099f25a4f599
Josh Myers-Dean <joshmyersdean@yahoo.com>
2019-12-10 11:10:04 -0800

    Finished day 2

--- day2/day2.py
+++ day2/day2.py
@@ -10,9 +10,29 @@ def partOne(nums):
         elif nums[i] == 2:
              nums[nums[i+3]] = nums[nums[i+1]] * nums[nums[i+2]]
 
+def partTwo(tmp):
+    target = 19690720
+    for i in range(100):
+        for j in range(100):
+            # reset the array
+            nums = tmp[:]
+            nums[1] = i
+            nums[2] = j
+            pointer = 0
+    	    for k in range(0, len(nums), 4):
+                if nums[k] == 99:
+                    break
+                if nums[k] == 1:
+                    nums[nums[k+3]] = nums[nums[k+1]] + nums[nums[k+2]]
+                elif nums[k] == 2:
+                    nums[nums[k+3]] = nums[nums[k+1]] * nums[nums[k+2]]
+            if nums[0] == target:
+                return 100*nums[1] + nums[2]
 
 if __name__ == '__main__':
     with open('input.txt', 'rt') as f_obj:
         f_split = [i.split(',') for i in f_obj][0][::-1]
-        nums = [int(i) for i in f_split][::-1]
-    print(partOne(nums))
+        num = [int(i) for i in f_split][::-1]
+        nums = num[:]
+    print(partOne(num))
+    print(partTwo(nums))
